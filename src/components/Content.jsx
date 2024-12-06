3a08afbe31b98f7029a56f4e9d4a7054b5e23168
Rragulr24 <ragulr63@gmail.com>
2024-12-06 23:34:48 +0530

    header

---
+++ src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const Content = () => {
+  return (
+    <div>
+        <h1>Hi, <br/> I'm R.Ragul
+        <p>I'm a FrontEnd Developer. </p></h1>
+        <img src={ContentImg}></img>
+    </div>
+  )
+}
+
+export default Content;
